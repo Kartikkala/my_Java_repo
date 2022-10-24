c68c177b1a61209f05b467780646865696e083ef
Kartikkala <kartikkala10december@gmail.com>
2022-10-24 09:15:04 +0530

    Added program to check if a number is Integer or not

---
+++ isInt.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class isInt {
+    public static void main(String [] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number bhomsdike: ");
+
+        System.out.printf("Hii lawde %d\n",4);
+        input.close();
+    }
+}
